6dc0c1b3b66ee98dc9d700606ecde0cc2e641597
Rahul <rahulanand2690@gmail.com>
2026-05-25 02:00:09 +0530

    data saved in database

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
 const connectDB =  require("./config/database");
-//require("./config/database");
-
 const {adminAuth} = require('./utils/auth.js');
+const User = require("./models/user.js");
+
+app.post("/signup", async(req, res) =>{
+ const userObj = {
+    firstName: "Rahul",
+    lastName: "Anand",
+    emailId: "ajghf@jkjdb.com",
+    password: "123456",
+    age: 24,   
+    gender: "Male" 
+ }
+
+ const user = new User(userObj);
+ try {
+    await user.save();
+ } catch (err) {
+    res.status(500).send("Error signing up user" + err.message);
+ }
+ 
+ res.send("User signed up successfully");
+});
+
 
 connectDB().
     then(() =>{
